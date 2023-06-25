b7cc76f3be8781a2f62863757cdfde4cc7a7ac1b
Melvin <melvingit@gmail.com>
2023-06-24 22:08:16 -0700

    submit problem

--- index.js
+++ index.js
@@ -95,6 +95,9 @@ snapshot.forEach((doc) => {
 
 });
 
+function onSubmit(){
+  console.log("submitted");
+}
 /*
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
